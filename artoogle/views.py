de3184f852107634a3555d08b7aa4d8ad41738f8
Kai <kai@naschinski.de>
2018-05-07 11:15:55 +0200

    autofocus on search bar

--- artoogle/views.py
+++ artoogle/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.template import loader
 
+
 def index(request):
     template = loader.get_template('artoogle/index.html')
     return HttpResponse(template.render())
 
+
+def search(request):
+    return HttpResponse(request)
