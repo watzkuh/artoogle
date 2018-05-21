3948ffc3b98506d99f51503dd403816742db4c4b
Kai <kai@naschinski.de>
2018-05-21 18:07:44 +0200

    prevent parsing of ontology for every request

--- artoogle/views.py
+++ artoogle/views.py
@@ -3,6 +3,7 @@ from django.template import loader
 import aai.suggestion as sgst
 import aai.query as query
 
+pool = query.RDFQueries()
 
 def index(request):
     template = loader.get_template('artoogle/index.html')
@@ -16,7 +17,8 @@ def search(request):
     if search_terms == 'bob ross':
         image_data = open("C:/Users/Naschinsui/PycharmProjects/aai/artoogle/static/artoogle/bob.jpg", "rb").read()
         return HttpResponse(image_data, content_type="image/png")
-    res = query.RDFQueries().get_artist(search_terms)
+
+    res = pool.get_artist(search_terms)
     return HttpResponse(res)
 
 
