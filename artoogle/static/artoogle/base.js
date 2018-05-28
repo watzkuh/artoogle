341601a9ed4d88c1cf5d0dfee9bc560e16fc014c
plunz <plunz@HDLNB268>
2018-05-28 10:22:28 +0200

    removed console logs

--- artoogle/static/artoogle/base.js
+++ artoogle/static/artoogle/base.js
@@ -5,7 +5,6 @@ var button;
 $(document).ready(function () {
     searchBox = $("#searchbox");
     button = $("#searchButton");
-    //searchBox.autocomplete({source: [ "c++", "java"]});
     searchBox.autocomplete({
         source: onSearch,
         autoFocus: true,
@@ -15,21 +14,16 @@ $(document).ready(function () {
 });
 
 function onSearch(request, response) {
-    console.log("suche nach:" + request.term);
     $.getJSON("/autosuggest", {"arg":encodeURI(request.term)}, function (data) {
-        console.log(data["suggestions"]);
         var suggestions = [];
         data["suggestions"].forEach(function(entry) {
             suggestions.push(decodeURI(entry));
         });
-        console.log(suggestions);
         response(suggestions);
 
     });
 }
 
 function onSelect(event, ui) {
-    console.log("clicked");
-    console.log(ui.item.value);
     searchBox.value = ui.item.value;
 }
